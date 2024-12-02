710e167e0fde263d69a9d5e8db7cbcd6ac69bd87
Valentin <v.stauber@parityqc.com>
2024-12-02 18:52:16 +0100

    day 1

---
+++ scripts/01.jl
@@ -0,0 +1,26 @@
+using CSV, DataFrames, DataStructures
+
+function absolute_difference_sum(df::DataFrame)
+
+    diffs = sort(df.a) - sort(df.b)
+    return sum(abs.(diffs))
+end
+
+function counter_similarity(df::DataFrame)
+    act = counter(df.a)
+    bct = counter(df.b)
+
+    sim = 0
+
+    for (a, ct) in act
+        sim += a * ct * get(bct, a, 0)
+    end
+    return sim
+end
+
+# Read CSV with space separators
+df = CSV.read("data/01.csv", DataFrame; delim=" ", ignorerepeated=true, header=[:a, :b])
+
+
+println("difference: $(absolute_difference_sum(df))")
+println("similarity: $(counter_similarity(df))")
