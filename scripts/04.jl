40597d7d04b064021a584f81c47708ba7e270b57
Valentin Stauber <v.stauber@parityqc.com>
2024-12-09 10:26:25 +0100

    04_1

--- scripts/04.jl
+++ scripts/04.jl
@@ -1,62 +1,47 @@
+DIRS = [
+    CartesianIndex(-1, 0),
+    CartesianIndex(-1, 1),
+    CartesianIndex(0, 1),
+    CartesianIndex(1, 1),
+    CartesianIndex(1, 0),
+    CartesianIndex(1, -1),
+    CartesianIndex(0, -1),
+    CartesianIndex(-1, -1)
+]
 
-function find_e(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-    word_len = length(word)
-    stop = col + word_len -1
+function get_candidate(charmat::Matrix, point::CartesianIndex, dir::CartesianIndex, n_chars::Int)
     try
-        candidates = charmat[row, col:stop]
-        return join(candidates) == word
-    catch e
-        return false
+        candidate = join([charmat[point+i*dir] for i in range(0, n_chars - 1)])
+        return candidate
+    catch BoundsError
+        return nothing
     end
 end
 
-function find_w(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-    word_len = length(word)
-    start = col - word_len + 1
-    try
-        candidates = charmat[row, start:col]
-        return join(reverse(candidates)) == word
-    catch e
-        return false
-    end
-end
-
-function find_n(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-end
-function find_s(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-end
-function find_ne(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-end
-function find_se(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-end
-function find_nw(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-end
-function find_sw(charmat::Matrix{Char}, word::String, row::Int, col::Int)
-end
-
-function find_all(charmat::Matrix{Char}, word::String)
-    rows, cols = size(charmat)
+function count_all(charmat::Matrix{Char}, word::String)
     ct = 0
     word_len = length(word)
-    for row in range(1, rows)
-        for col in range(1, cols - word_len)
-            # stop = min(cols, (col+word_len-1))
-            # candidate = join(charmat[row, col: col + word_len-1])
-            if (find_e(charmat, word, row, col) || find_w(charmat, word, row, col))
-                println("($row, $col)")
+    indices = CartesianIndices(charmat)
+    for i in eachindex(charmat)
+        p = indices[i]
+        for dir in DIRS
+            candidate = get_candidate(charmat, p, dir, word_len)
+            if candidate == word
                 ct += 1
             end
         end
     end
+    ct
 end
 
 function main()
-    lines = open("data/04_test.txt") do f
+    lines = open("data/04.txt") do f
         readlines(f)
     end
     # https://discourse.julialang.org/t/how-to-convert-vector-of-vectors-to-matrix/72609
     charmat = mapreduce(permutedims, vcat, collect.(lines))
-    find_all(charmat, "XMAS")
+    word = "XMAS"
+    println("matches for '$word': $(count_all(charmat, word))")
 end
 
 main()
